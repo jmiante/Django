ac761e80fa648bed4ddf3f4309a8668ba3ee56c8
Jonathas Henrique <42851259+jhmiante@users.noreply.github.com>
2022-04-27 22:30:12 -0300

    Django Básico - Verificando se Usuário está Logado

--- Curso Django Essencial/03. Django Básico/core/views.py
+++ Curso Django Essencial/03. Django Básico/core/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 
 
 def index(request):
+    print(request.user)
+
+    if str(request.user) == 'AnonymousUser':
+        teste = 'Usuário Não Logado'
+    else:
+        teste = f'Bem vindo {request.user}'
+        
     context = {
         'curso': 'Programação com Web com Django Frameword',
-        'outro': 'Curso'
+        'outro': 'Curso',
+        'user': teste
     }
     return render(request, 'index.html', context)
 
