f937070e9ebde95b8cf38b7ee72328737b992236
Jonathas Henrique <42851259+jhmiante@users.noreply.github.com>
2022-04-27 21:18:39 -0300

    Django Básico - Passando Dados Backend para o Template

--- Curso Django Essencial/03. Django Básico/core/views.py
+++ Curso Django Essencial/03. Django Básico/core/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'curso': 'Programação com Web com Django Frameword',
+        'outro': 'Curso'
+    }
+    return render(request, 'index.html', context)
 
 
 def contato(request):
