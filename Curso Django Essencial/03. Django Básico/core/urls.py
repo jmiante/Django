ec112a01011bb8a1f9fd641efe8746110d550c9f
Jonathas Henrique <42851259+jhmiante@users.noreply.github.com>
2022-04-27 21:47:59 -0300

    Django Básico - Criando Usuário SuperUser e corrigindo Rotas {} por []

--- Curso Django Essencial/03. Django Básico/core/urls.py
+++ Curso Django Essencial/03. Django Básico/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import index, contato
 
-urlpatterns = {
+urlpatterns = [
     path('', index),
-    path('contato/', contato)
-}
+    path('contato/', contato),
+]
