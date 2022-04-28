372eef8275b0b02310bb96ae97c1633fc5009fe8
Jonathas Henrique <42851259+jhmiante@users.noreply.github.com>
2022-04-27 21:35:12 -0300

    Django Básico - Segundo Modelo 'Cliente'

--- Curso Django Essencial/03. Django Básico/core/models.py
+++ Curso Django Essencial/03. Django Básico/core/models.py
@@ -5,3 +5,9 @@ class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=10)
     estoque = models.IntegerField('Quantidade em Estoque')
+
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
