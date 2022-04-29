36ec8bc710ee68fb2e3584cee72691f8b8850602
Jonathas Henrique <42851259+jhmiante@users.noreply.github.com>
2022-04-28 22:21:56 -0300

    Django Básico - Tratando Página Não encontrada 404 e 500

--- Curso Django Essencial/03. Django Básico/core/views.py
+++ Curso Django Essencial/03. Django Básico/core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
+
 from .models import Produto
+
 
 def index(request):
 
@@ -24,8 +29,20 @@ def contato(request):
 
 
 def produto(request, pk):
-    prod = Produto.objects.get(pk=pk)
+    #prod = Produto.objects.get(pk=pk)
+    prod = get_object_or_404(Produto, id=pk)
+
     context = {
         'produto': prod,
     }
     return render(request, 'produto.html', context)
+
+
+def error404(request, exception):
+    template = loader.get_template('error404.html')
+    return HttpResponse(content=template.render(), content_type='text/html', charset='utf-8', status=404)
+
+
+def error500(request):
+    template = loader.get_template('error500.html')
+    return HttpResponse(content=template.render(), content_type='text/html', charset='utf-8', status=500)
