c0e783a37a5ce4f6a8e7f29194f9202c65870dad
Jonathas Henrique <jonathasme@gmail.com>
2022-05-08 22:44:41 -0300

    Registrando Autor com Funções Personalizadas

--- Curso Django Essencial/06. Django UserModel/core/admin.py
+++ Curso Django Essencial/06. Django UserModel/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titulo', '_autor')
+
+    def _autor(self, instance):
+        return f'{instance.autor.get_full_name()}'
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(autor=request.user)
