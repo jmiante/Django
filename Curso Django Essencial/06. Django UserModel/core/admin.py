50ce41f037af13f389bddf110ced94efce07a15e
Jonathas Henrique <jonathasme@gmail.com>
2022-05-08 22:48:17 -0300

    Excluindo Campo e incluindo automaticamente os dados de Usuário

--- Curso Django Essencial/06. Django UserModel/core/admin.py
+++ Curso Django Essencial/06. Django UserModel/core/admin.py
@@ -5,6 +5,7 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('titulo', '_autor')
+    exclude = ['autor']
 
     def _autor(self, instance):
         return f'{instance.autor.get_full_name()}'
@@ -12,3 +13,7 @@ class PostAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         qs = super(PostAdmin, self).get_queryset(request)
         return qs.filter(autor=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.autor = request.user
+        super().save_model(request, obj, form, change)
