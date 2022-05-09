05b09ab3ba7b641f6919535372300b15cd636238
Jonathas Henrique <jonathasme@gmail.com>
2022-05-08 22:51:14 -0300

    Alterando Dados do THEMA do ADMIN

--- Curso Django Essencial/06. Django UserModel/djangousermodel/urls.py
+++ Curso Django Essencial/06. Django UserModel/djangousermodel/urls.py
@@ -19,3 +19,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = 'Estudo Django'
+admin.site.site_title = 'Evoluindo em Django'
+admin.site.index_title = 'Model User Django'
