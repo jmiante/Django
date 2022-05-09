851f9ee0a705bd100f290e69c20a3ffdd8fa00cd
Jonathas Henrique <jonathasme@gmail.com>
2022-05-08 23:32:58 -0300

    Criando Usuário Customizado

--- Curso Django Essencial/06. Django UserModel/core/admin.py
+++ Curso Django Essencial/06. Django UserModel/core/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Post
+from django.contrib.auth.admin import UserAdmin
+#from .models import Post,
+from .models import CustomUsuario
+from .forms import CustomUsuarioCreateForm, CustomUsuarioChangeForm
 
-
+"""
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('titulo', '_autor')
@@ -17,3 +20,17 @@ class PostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.autor = request.user
         super().save_model(request, obj, form, change)
+
+"""
+@admin.register(CustomUsuario)
+class CustomUsuarioAdmin(UserAdmin):
+    add_form = CustomUsuarioCreateForm
+    form = CustomUsuarioChangeForm
+    model = CustomUsuario
+    list_display = ('first_name', 'last_name', 'email', 'fone', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Informações Pessoais', {'fields': ('first_name', 'last_name', 'fone')}),
+        ('Permissões', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
+    )
