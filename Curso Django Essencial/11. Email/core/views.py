5cb67a8d5aa5ce0c982a117a10403f142b7ceca5
Jonathas Henrique <jonathasme@gmail.com>
2022-05-18 22:17:49 -0300

    Send Mail com Anexos

--- Curso Django Essencial/11. Email/core/views.py
+++ Curso Django Essencial/11. Email/core/views.py
@@ -2,7 +2,9 @@ from django.contrib import messages
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.conf import settings
+
 from .forms import SubscribeForm
+from django.core.mail import EmailMessage
 
 
 def subscribe(request):
@@ -13,8 +15,20 @@ def subscribe(request):
             subject = 'Code Band'
             message = 'Sending Email through Gmail'
             recipient = form.cleaned_data.get('email')
-            send_mail(subject,
-              message, settings.EMAIL_HOST_USER, [recipient], fail_silently=False)
+
+            email = EmailMessage(
+                subject,
+                message,
+                settings.EMAIL_HOST_USER,
+                [recipient],
+                reply_to=[settings.EMAIL_HOST_USER],
+                headers={'Message-ID': 'foo'},
+            )
+
+            email.attach_file(r'C:\relatorio1.pdf')
+
+            email.send()
+
             messages.success(request, 'Success!')
             return redirect('home')
     return render(request, 'subscriptions/home.html', {'form': form})
